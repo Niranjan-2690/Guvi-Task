021baf8239880c31dbf9cf0e89a51aab00d59bd3
Niranjan-2690 <niranjan.niran4@gmail.com>
2024-05-30 01:10:28 +0530

    Day4 Task

--- 27-may-2024/task1.js
+++ 27-may-2024/task1.js
@@ -1,1 +1,27 @@
 // Do the below programs in anonymous function & IIFE
+
+// a. Print odd numbers in an array
+
+    var arrAno = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var oddNumbers = function (oddNumber){
+        let result = []
+        oddNumber.forEach((ele)=>{
+            if(ele % 2 !==0){
+                result.push(ele)
+            }
+        })
+        return result
+    }
+    console.log(oddNumbers(arrAno))
+
+    var arrIIFE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    (function (oddNumber){
+        let result = []
+        oddNumber.forEach((ele)=>{
+            if(ele % 2 !==0){
+                result.push(ele)
+            }
+        })
+        return result
+    })
+    console.log(oddNumbers(arrIIFE))
