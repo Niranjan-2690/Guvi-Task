999feaf23cb0fad9f9c759dfcce56798fedd74ef
Niranjan-2690 <niranjan.niran4@gmail.com>
2024-05-30 23:23:34 +0530

    Task

--- 27-may-2024/task1.js
+++ 27-may-2024/task1.js
@@ -26,7 +26,9 @@
     })
     console.log(oddNumbers(arrIIFE))
 
-//b. Convert all the strings to title caps in a string array
+//--------------------------------------------------------------------------------------
+
+// b. Convert all the strings to title caps in a string array
 
     let titles = ["Captain America", "Iron Man", "Spider Man", "Black Panther"]
     let initialCapLetter = function(titles){
@@ -44,4 +46,45 @@
     })
     console.log(initialCapLetterIIFE(titlesIIFE))
 
+//--------------------------------------------------------------------------------------
 
+// c.  Sum of all numbers in an array
+
+    let sum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let sumNumbers = function(number){
+        let value = 0; 
+        for (let x = 0; x < number.length; x++){
+            value += number[x]
+        }
+        return value
+    }
+    console.log(sumNumbers(sum))
+
+
+    let sumIIFE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let sumNumbersIIFE = (function(number){
+        let value = 0; 
+        for (let x = 0; x < number.length; x++){
+            value += number[x]
+        }
+        return value
+    })
+    console.log(sumNumbersIIFE(sum))
+
+//--------------------------------------------------------------------------------
+
+// d. Return all the prime numbers in an array
+
+    let isPrime = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    let prime = function(priNumbers){
+        if(priNumbers <= 1){
+            return false
+        }if(priNumbers === 2){
+            return true
+        }if(priNumbers % 2 !==0){
+            return false
+        }
+    }
+    console.log(prime(isPrime))
