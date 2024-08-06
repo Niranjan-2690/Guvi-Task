a8194826a6958cdc2f6481fdf432d2b5be335a69
Niranjan-2690 <niranjan.niran4@gmail.com>
2024-08-06 23:04:32 +0530

    Task 1

--- 13-june-2024/task-1/script.js
+++ 13-june-2024/task-1/script.js
@@ -1,5 +1,25 @@
-const values = document.getElementById(nums)
-const inputs = values.addEventListener("click", (e)=>{
-    e.target.value
-})
-console.log(inputs)
+function appendToDisplay(nums) {
+    document.getElementById("display").value += nums;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteValue() {
+    document.getElementById("display").value = display.value.slice(0, -1)
+}
+
+function calculate() {
+    const display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert("Invalid input");
+        document.getElementById("display").value = "";
+    }
+}
+
+function percentage(){
+    document.getElementById('display').value = (display.value)/100
+}
