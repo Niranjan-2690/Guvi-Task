d1554a998aae5c0e768aedd8b68e31ba812b29f5
Niranjan-2690 <niranjan.niran4@gmail.com>
2024-08-10 16:35:13 +0530

    task

--- 13-june-2024/task-2/script.js
+++ 13-june-2024/task-2/script.js
@@ -1,14 +1,32 @@
+let serialNumber = 1
 document.getElementById("myForm").addEventListener("submit", (e)=>{
     const firstname = document.getElementById("firstname").value
     const lastname = document.getElementById("lastname").value
-    console.log("Firstname: ", firstname)
-    console.log("Lastname: ", lastname)
+    const email = document.getElementById("email").value
+    const address = document.getElementById("address").value
+    const city = document.getElementById("city").value
+    const state = document.getElementById("state").value
+    const postcode = document.getElementById("postcode").value
+    const gender = document.getElementById("gender").value
+    const foods = Array.from(document.getElementsByName("foods"))
+                        .filter(input => input.checked)
+                        .map(input => input.value)
+                        .join(", ");
+
     e.preventDefault()
 
-    let serialNumber = 1
     const tableRow = document.createElement("tr")
     tableRow.innerHTML = `<th scope="row">${serialNumber}</th>
-                          <td>${firstname}</td>`
+                          <td>${firstname}</td>
+                          <td>${lastname}</td>
+                          <td>${email}</td>
+                          <td>${address}</td>
+                          <td>${city}</td>
+                          <td>${state}</td>
+                          <td>${postcode}</td>
+                          <td>${gender}</td>
+                          <td>${foods}</td>`
     document.getElementById("myTable").appendChild(tableRow);
     serialNumber++;
+    document.getElementById("myForm").reset()
 })
